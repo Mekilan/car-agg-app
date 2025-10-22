a7aa53295e2ea13ca75dc07fb02d6be0fe273106
Mekilan <50459351+Mekilan@users.noreply.github.com>
2025-10-22 17:33:30 +0000

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
     strictPort: true,
     allowedHosts: true,
     hmr: {
       clientPort: 443,
-      protocol: 'wss'
-    }
-  }
-})
+      protocol: "wss",
+    },
+  },
+});
